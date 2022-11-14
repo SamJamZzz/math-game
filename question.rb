a7d5ace6455e13d0b1b239733fac9e650a39cb4c
Sam <samjulian6@gmail.com>
2022-11-14 10:50:34 -0800

    Finish implementing Question class

--- question.rb
+++ question.rb
@@ -1,12 +1,25 @@
 class Question
+  attr_accessor :num1, :num2, :answer
 
   def initialize(num1, num2)
     @num1 = num1
     @num2 = num2
+    @answer = nil
   end
 
   def correct_answer
     @num1 + @num2
   end
 
+  def answer(ans)
+    @answer = ans
+  end
+
+  def answered_correctly?()
+    self.correct_answer == @answer
+  end
+
+  def question
+    "What does #{num1} plus #{num2} equal?"
+  end
 end
